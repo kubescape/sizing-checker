6a57be537b60d849958025a5b4195745af93b3a1
DanDavidA <166003027+DanDavidA@users.noreply.github.com>
2025-03-16 14:16:43 +0200

    Added a parameter to set a custom kubeconfig file path (--kubeconfig)
    Signed-off-by: David Alima <davida@armosec.io>

--- pkg/common/client.go
+++ pkg/common/client.go
@@ -10,20 +10,28 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-func BuildKubeClient() (*kubernetes.Clientset, bool) {
+func BuildKubeClient(kubeconfigPath string) (*kubernetes.Clientset, bool) {
 	inCluster := true
+
+	// Try in-cluster config first
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		// Fallback to local kubeconfig => we're not in-cluster
+		// Not running in a cluster, so fallback to local kubeconfig
 		inCluster = false
-		kubeconfig := filepath.Join(os.Getenv("HOME"), ".kube", "config")
-		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+
+		// If user did not provide --kubeconfig, fallback to ~/.kube/config
+		if kubeconfigPath == "" {
+			kubeconfigPath = filepath.Join(os.Getenv("HOME"), ".kube", "config")
+		}
+
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfigPath)
 		if err != nil {
 			log.Printf("Could not load in-cluster or local kubeconfig: %v", err)
 			return nil, inCluster
 		}
 	}
 
+	// Build the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		log.Printf("Failed to create Kubernetes clientset: %v", err)
